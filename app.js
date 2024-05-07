fbdb318e01a8100d5558e2ea569b9d98ed72aa66
Caleb Oliver <oliverc@gonzaga.org>
2024-05-07 10:50:09 -0400

    Create Date Variable name

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.get('/Create', function(req, res){
 
 
 app.post('/apex', (req, res) => {
-    const { name, sub, sec, ass, qot} = req.body;
+    const { name, sub, sec, ass, qot, day} = req.body;
 
     const apexList = db.get('apexList') || [];
 
-    apexList.push({name, sub, sec, ass, qot});
+    apexList.push({name, sub, sec, ass, qot, day});
 
     db.set('apexList', apexList);
 
@@ -64,10 +64,10 @@ app.get('/view/:id', (req, res) => {
 
 app.post('/edit/:id', (req,res) => {
     const id = req.params.id;
-    const { name, sub, sec, ass, qot} = req.body;
+    const { name, sub, sec, ass, qot, day} = req.body;
 
     const apexList = db.get('apexList') || [];
-    apexList[id] = { name, sub, sec, ass, qot};
+    apexList[id] = { name, sub, sec, ass, qot, day};
     db.set('apexList', apexList);
 
     res.redirect('/view/'+id);
